4517e137999010b6a2d0f93e85a21c5a0e91df8f
Yoboinef-2000 <135835812+Yoboinef-2000@users.noreply.github.com>
2024-11-26 16:29:14 -0500

    The routes issue should hopefully be taken care of now.

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,47 +1,16 @@
-// import axios from 'axios';
-
-// const instance = axios.create({
-//   baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api',
-//   withCredentials: true,
-// });
-
-// // Add request interceptor to add token
-// instance.interceptors.request.use(
-//   (config) => {
-//     const token = localStorage.getItem('token');
-//     if (token) {
-//       config.headers['Authorization'] = `Bearer ${token}`;
-//     }
-//     return config;
-//   },
-//   (error) => {
-//     return Promise.reject(error);
-//   }
-// );
-
-// // Add response interceptor to handle auth errors
-// instance.interceptors.response.use(
-//   (response) => response,
-//   async (error) => {
-//     if (error.response?.status === 401) {
-//       localStorage.removeItem('token');
-//       window.location.href = '/login';
-//     }
-//     return Promise.reject(error);
-//   }
-// );
-
-// export default instance;
-
 import axios from 'axios';
 
+// const baseURL = `${process.env.REACT_APP_API_URL}/api`;
+const baseURL = process.env.REACT_APP_API_URL;
+
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api',
+  baseURL,
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json'
-  }
+  },
+  timeout: 10000 // 10 second timeout
 });
 
 // Add request interceptor to add token
@@ -51,8 +20,6 @@ instance.interceptors.request.use(
     if (token) {
       config.headers['Authorization'] = `Bearer ${token}`;
     }
-    // Remove credentials header if it exists
-    delete config.headers['Access-Control-Allow-Credentials'];
     return config;
   },
   (error) => {
